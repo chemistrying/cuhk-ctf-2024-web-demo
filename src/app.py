039b144fb6b0aac721c611002097b2399f7aa1d0
chemistrying <toto4801110@gmail.com>
2024-09-26 12:41:22 +0800

    :ambulance: Decrease time limit for processes

--- src/app.py
+++ src/app.py
@@ -132,7 +132,7 @@ def code():
                 f.write(code)
                 f.flush()
 
-            completed_process = subprocess.run(["python", f"/tmp/{id}.py"], timeout=2, capture_output=True)
+            completed_process = subprocess.run(["python", f"/tmp/{id}.py"], timeout=0.5, capture_output=True)
             return render_template("code.html", returncode=completed_process.returncode, output=completed_process.stdout.decode(), error=completed_process.stderr.decode())
 
         return render_template("code.html", hint="Invalid admin password!")
